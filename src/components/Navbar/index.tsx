2127264969f750c9ced6e314d4483bf1e58e2469
Levente Otta <leventeotta@gmail.com>
2024-10-12 19:46:32 +0200

    Add navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,77 @@
+import MobileMenu from "@/components/Navbar/mobile-menu";
+import ThemeSwitcherButton from "@/components/Navbar/theme-switcher-button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import Link from "next/link";
+import { FaAlignRight, FaLinkedin, FaRegFilePdf, FaSquareGithub } from "react-icons/fa6";
+
+export default function Navbar() {
+  return (
+    <nav className="m-4 flex h-20 items-center gap-2 md:gap-6 lg:gap-8">
+      <Link
+        href="/"
+        className="border-border bg-accent/30 hover:bg-accent inline-flex h-full items-center rounded-xl border-2 px-4 text-center text-2xl font-bold transition-all hover:border-orange-500 md:px-8 lg:px-16"
+      >
+        Levente Otta
+      </Link>
+      <div className="flex-1"></div>
+      <ThemeSwitcherButton />
+      <div className="hidden h-full gap-4 md:inline-flex">
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <a
+                href="https://github.com/Otisz"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-accent/30 hover:bg-accent border-border inline-flex aspect-square h-full items-center justify-center rounded-xl border-2 transition-all hover:border-orange-500"
+                aria-label="Check out my Github"
+              >
+                <FaSquareGithub className="h-8 w-8" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Check out my Github</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <a
+                href="https://www.linkedin.com/in/leventeotta/"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-accent/30 hover:bg-accent border-border inline-flex aspect-square h-full items-center justify-center rounded-xl border-2 transition-all hover:border-orange-500"
+                aria-label="Visit my LinkedIn"
+              >
+                <FaLinkedin className="h-8 w-8" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Visit my LinkedIn</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <a
+                href="https://assets.leventeotta.com/documents/Levente%20Otta%20CV.pdf"
+                target="_blank"
+                rel="noreferrer"
+                className="bg-accent/30 hover:bg-accent border-border inline-flex aspect-square h-full items-center justify-center rounded-xl border-2 transition-all hover:border-orange-500"
+                aria-label="Download my CV"
+              >
+                <FaRegFilePdf className="h-8 w-8" />
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Download my CV</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+      <MobileMenu />
+    </nav>
+  );
+}
---
+++ src/components/Navbar/mobile-menu.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import Link from "next/link";
+import { useState } from "react";
+import { FaAlignRight, FaLinkedin, FaRegCircleXmark, FaRegFilePdf, FaSquareGithub } from "react-icons/fa6";
+
+export default function MobileMenu() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger
+        className="bg-accent/30 hover:bg-accent border-border inline-flex aspect-square h-full items-center justify-center justify-self-end rounded-xl border-2 transition-all hover:border-orange-500 md:hidden"
+        aria-label="Open navigation menu"
+      >
+        <FaAlignRight className="h-8 w-8" />
+      </SheetTrigger>
+      <SheetContent noClose className="w-full border-none p-0">
+        <SheetHeader className="divide-background m-4 flex h-20 flex-row divide-x">
+          <SheetTitle className="flex flex-1 items-center px-8 text-2xl font-bold">Menu</SheetTitle>
+          <SheetClose
+            className="bg-accent/30 hover:bg-accent border-border inline-flex aspect-square h-full items-center justify-center rounded-xl border-2 transition-all hover:border-orange-500"
+            aria-labelledby="close-navitaion-menu"
+          >
+            <FaRegCircleXmark className="h-8 w-8" />
+            <span className="sr-only" id="close-navitaion-menu">
+              Close navigation menu
+            </span>
+          </SheetClose>
+          <VisuallyHidden asChild>
+            <SheetDescription className="hidden">Navigation menu</SheetDescription>
+          </VisuallyHidden>
+        </SheetHeader>
+        <div className="h-full space-y-8 overflow-auto pb-32">
+          <div className="divide-accent flex flex-col divide-y"></div>
+          <div className="grid grid-cols-2 gap-4 px-8">
+            <a
+              href="https://assets.leventeotta.com/documents/Levente%20Otta%20CV.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="col-span-2 inline-flex flex-1 items-center justify-center gap-4 rounded-lg border-2 border-orange-500 p-8"
+              aria-label="Download my CV"
+            >
+              <FaRegFilePdf className="h-6 w-6 fill-orange-500" />
+              <span>Download CV</span>
+            </a>
+            <a
+              href="https://github.com/Otisz"
+              target="_blank"
+              rel="noreferrer"
+              className="border-border inline-flex flex-1 items-center justify-center rounded-lg border-2 p-8"
+              aria-label="Check out my Github"
+            >
+              <FaSquareGithub className="h-8 w-8" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/leventeotta/"
+              target="_blank"
+              rel="noreferrer"
+              className="border-border inline-flex flex-1 items-center justify-center rounded-lg border-2 p-8"
+              aria-label="Visit my LinkedIn"
+            >
+              <FaLinkedin className="h-8 w-8" />
+            </a>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
