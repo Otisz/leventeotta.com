a4814305974b485519610e60f61c34f7ba5e190d
Levente Otta <leventeotta@gmail.com>
2024-10-12 15:56:00 +0200

    Fresh NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    TOOLTIP_DELAY_DURATION: "150",
-  },
-  swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  experimental: {
+    reactCompiler: true
+  }
 };
 
 export default nextConfig;
