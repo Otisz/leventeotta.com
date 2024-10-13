9ce5512d9707c3ab472c7add16703e4243de7142
Levente Otta <leventeotta@gmail.com>
2024-10-13 11:39:30 +0200

    Skip checks during builnd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
